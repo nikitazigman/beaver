ba5b6395c07094400f575df4ab2ea959ae6784ef
nikita zigman <zigman.nikita@gmail.com>
2023-12-30 15:34:30 +0000

    feat beaver_etl: moves parsers module to the utils folder

--- backend/apps/beaver_etl/beaver_etl/utils/project_parsers/python_project_parsers.py
+++ backend/apps/beaver_etl/beaver_etl/utils/project_parsers/python_project_parsers.py
@@ -3,13 +3,13 @@ import tomllib
 from abc import ABC, abstractmethod
 from pathlib import Path
 
-from beaver_etl.schemas.extract_schema import InCodeSchema
+from backend.apps.beaver_etl.beaver_etl.schemas.parser import InCodeSchema
 
 
 class IPythonProjectParser(ABC):
     @abstractmethod
     def retrieve_data(self, project: Path) -> InCodeSchema:
-        """Removes docstrings and comments from the code."""
+        """retrieves data from a python project"""
 
 
 class PoetryProjectParser(IPythonProjectParser):
@@ -24,13 +24,13 @@ class PoetryProjectParser(IPythonProjectParser):
         self.relative_path_to_readme = relative_path_to_readme
 
     def retrieve_data(self, project: Path) -> InCodeSchema:
-        poetry_project_config = self.toml_to_dict(project)
-        link_to_task = self.get_link_to_task(poetry_project_config)
-        title = self.get_title(poetry_project_config)
-        types = self.get_types(poetry_project_config)
+        poetry_project_config = self._toml_to_dict(project)
+        link_to_task = self._get_link_to_task(poetry_project_config)
+        title = self._get_title(poetry_project_config)
+        types = self._get_types(poetry_project_config)
 
-        source_code = self.get_source_code(project)
-        readme = self.get_readme(project)
+        source_code = self._get_source_code(project)
+        readme = self._get_readme(project)
 
         code_schema = InCodeSchema(
             source_code=source_code,
@@ -42,22 +42,22 @@ class PoetryProjectParser(IPythonProjectParser):
         )
         return code_schema
 
-    def toml_to_dict(self, project: Path) -> dict:
+    def _toml_to_dict(self, project: Path) -> dict:
         return tomllib.loads(
             (project / self.relative_path_to_pyproject_toml).read_text()
         )
 
-    def get_link_to_task(self, poetry_config: dict) -> str:
+    def _get_link_to_task(self, poetry_config: dict) -> str:
         return poetry_config["tool"]["poetry"]["documentation"]
 
-    def get_title(self, poetry_config: dict) -> str:
+    def _get_title(self, poetry_config: dict) -> str:
         return poetry_config["tool"]["poetry"]["description"]
 
-    def get_types(self, poetry_config: dict) -> list[str]:
+    def _get_types(self, poetry_config: dict) -> list[str]:
         return poetry_config["tool"]["poetry"]["keywords"]
 
-    def get_source_code(self, project: Path) -> str:
+    def _get_source_code(self, project: Path) -> str:
         return (project / self.relative_path_to_main).read_text()
 
-    def get_readme(self, project: Path) -> str:
+    def _get_readme(self, project: Path) -> str:
         return (project / self.relative_path_to_readme).read_text()
