243789c42ae87019e19102a3a524d78fa236e452
nikita zigman <zigman.nikita@gmail.com>
2023-12-31 15:39:13 +0000

    feat dataset: add remove duplicates task to the dataset

---
+++ dataset/algorithms/Arrays/RemoveDuplicatesFromSortedArrayII/tests/test_main.py
@@ -0,0 +1,15 @@
+import pytest
+
+from src.main import Solution
+
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    (
+        ([1, 1, 1, 2, 2, 3], [1, 1, 2, 2, 3]),
+        ([0, 0, 1, 1, 1, 1, 2, 3, 3], [0, 0, 1, 1, 2, 3, 3]),
+    ),
+)
+def test_main(nums: list[int], expected: list[int]):
+    array_length = Solution().removeDuplicates(nums)
+    assert nums[:array_length] == expected
