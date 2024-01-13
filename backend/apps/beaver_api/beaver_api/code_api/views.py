675871bf901644010babb7a66e874706a477002a
nikita zigman <zigman.nikita@gmail.com>
2024-01-13 23:46:36 +0000

    feat beaver_api: adds basic retrieve api

--- backend/apps/beaver_api/beaver_api/code_api/views.py
+++ backend/apps/beaver_api/beaver_api/code_api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from code_api.models import CodeDocument
+from code_api.serializers import (
+    CodeDocumentSerializer,
+)
 
-# Create your views here.
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+
+
+class RetrieveCodeDocumentView(RetrieveAPIView):
+    queryset = CodeDocument.objects.all()
+    serializer_class = CodeDocumentSerializer
+
+
+class ListCodeDocumentView(ListAPIView):
+    queryset = CodeDocument.objects.all()
+    serializer_class = CodeDocumentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["language__name", "tags__name"]
