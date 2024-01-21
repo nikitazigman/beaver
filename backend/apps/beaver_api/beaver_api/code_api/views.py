aaaff6e77be1813d2795ede9d821355244b316f7
nikita zigman <zigman.nikita@gmail.com>
2024-01-21 22:02:20 +0000

    feat beaver_api: adds view to get random code document

--- backend/apps/beaver_api/beaver_api/code_api/views.py
+++ backend/apps/beaver_api/beaver_api/code_api/views.py
@@ -1,19 +1,21 @@
+from code_api.mixins import GetRandomObjectMixin
 from code_api.models import CodeDocument
 from code_api.serializers import (
     CodeDocumentSerializer,
 )
 
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import GenericAPIView
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 
-class RetrieveCodeDocumentView(RetrieveAPIView):
-    queryset = CodeDocument.objects.all()
-    serializer_class = CodeDocumentSerializer
-
-
-class ListCodeDocumentView(ListAPIView):
+class GetRandomCodeDocumentView(GenericAPIView, GetRandomObjectMixin):
     queryset = CodeDocument.objects.all()
     serializer_class = CodeDocumentSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["language__name", "tags__name"]
+    pagination_class = None
+
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        return self.get_random_document(request, *args, **kwargs)
