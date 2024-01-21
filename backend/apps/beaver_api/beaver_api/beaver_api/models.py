d9a03bfb35802fc81d118c2dba4e9ecfbf5523f8
nikita zigman <zigman.nikita@gmail.com>
2024-01-21 22:00:39 +0000

    feat beaver_api: adds type annotation for models

--- backend/apps/beaver_api/beaver_api/beaver_api/models.py
+++ backend/apps/beaver_api/beaver_api/beaver_api/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 
 class UUIDMixin(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False)
+    id: models.UUIDField = models.UUIDField(primary_key=True, editable=False)
 
     class Meta:
         abstract = True
 
 
 class TimeStampMixin(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True)
+    updated_at: models.DateTimeField = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
