536cb6f33d93c90f19838f8be8fb83f58b7d0e69
nikita zigman <zigman.nikita@gmail.com>
2024-01-21 22:01:47 +0000

    feat beaver_api: updates urls for the code_api

--- backend/apps/beaver_api/beaver_api/code_api/urls.py
+++ backend/apps/beaver_api/beaver_api/code_api/urls.py
@@ -1,17 +1,12 @@
-from code_api.views import ListCodeDocumentView, RetrieveCodeDocumentView
+from code_api.views import GetRandomCodeDocumentView
 
 from django.urls import path
 
 
 urlpatterns = [
     path(
-        "code_documents/<uuid:pk>/",
-        RetrieveCodeDocumentView.as_view(),
-        name="code_document",
-    ),
-    path(
         "code_documents/",
-        ListCodeDocumentView.as_view(),
+        GetRandomCodeDocumentView.as_view(),
         name="code_documents",
     ),
 ]
