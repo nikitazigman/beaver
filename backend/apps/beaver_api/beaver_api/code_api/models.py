1c5ea978524478e4cd4506cc096c60ae72b9c44c
nikita zigman <zigman.nikita@gmail.com>
2024-01-21 22:00:39 +0000

    feat beaver_api: adds type annotation for models

--- backend/apps/beaver_api/beaver_api/code_api/models.py
+++ backend/apps/beaver_api/beaver_api/code_api/models.py
@@ -1,23 +1,29 @@
 from beaver_api.models import TimeStampMixin, UUIDMixin
 from django.db import models
+from language_api.models import Language
+from tags_api.models import Tag
 
 
-class CodeDocument(UUIDMixin, TimeStampMixin):
-    title = models.CharField(
+class CodeDocument(UUIDMixin, TimeStampMixin):  # type: ignore
+    title: models.CharField = models.CharField(
         max_length=255, blank=False, null=False, unique=True
     )
-    code = models.TextField(blank=False, null=False, unique=True)
-    link_to_project = models.URLField(blank=False, null=False, unique=True)
+    code: models.TextField = models.TextField(
+        blank=False, null=False, unique=True
+    )
+    link_to_project: models.URLField = models.URLField(
+        blank=False, null=False, unique=True
+    )
 
-    language = models.ForeignKey(
-        "language_api.Language",
+    language: models.ForeignKey = models.ForeignKey(
+        Language,
         on_delete=models.DO_NOTHING,
         related_name="code_documents",
         null=True,
     )
-    tags = models.ManyToManyField(
-        "tags_api.Tag", related_name="code_documents"
+    tags: models.ManyToManyField = models.ManyToManyField(
+        Tag, related_name="code_documents"
     )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.language}/{self.title}"
