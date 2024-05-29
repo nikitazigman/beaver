bb71728dff88c20a9f8a350692f78aedcebfe152
nikita zigman <zigman.nikita@gmail.com>
2024-05-29 23:37:13 +0200

    Add services

--- backend/apps/beaver_api/beaver_api/code_api/services.py
+++ backend/apps/beaver_api/beaver_api/code_api/services.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from random import choice
 from typing import TypeVar
 from uuid import UUID
@@ -19,3 +21,26 @@ def get_random_object_id(queryset: QuerySet) -> UUID:
 
     random_id = choice(objects_ids)
     return random_id
+
+
+def delete_objects_by_ids(queryset: QuerySet, ids: list[UUID]) -> None:
+    queryset.filter(id__in=ids).delete()
+
+
+def compute_hash(code: str) -> dict:
+    encoded_code_content = code.encode("utf-8")
+    return hashlib.sha256(encoded_code_content).hexdigest()
+
+
+def get_or_create_obj_by_names[T: Model](
+    names: list[str], model_class: T
+) -> list[T]:
+    existing_objs = model_class.objects.filter(name__in=names)
+    existing_names = existing_objs.values_list("name", flat=True)
+
+    new_names = set(names) - set(existing_names)
+
+    new_obj_instances = [model_class(name=name) for name in new_names]
+    new_objs = model_class.objects.bulk_create(new_obj_instances)
+
+    return list(existing_objs) + new_objs
