9c41a04c1387ad3885c1f6d09faf0c4ab00d8582
nikita zigman <zigman.nikita@gmail.com>
2024-08-05 19:43:47 +0100

    Add scripts to build, push and auth docker cli

---
+++ IaC/scripts/build_docker_api.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Determine the current script's directory
+this_file=$(realpath $0)
+this_dir=$(dirname $this_file)
+# Build the path to the API Application's base directory
+api_dir=$(realpath $this_dir/../../)
+
+docker build -t public.ecr.aws/d0s9n5w1/beaver-api:latest $api_dir/backend/apps/beaver_api
