d764b67797080f55adae03f87e3fb3f8ae727868
nikita zigman <zigman.nikita@gmail.com>
2024-08-27 22:38:16 +0100

    Add scripts to build and push containers to aws registry

--- IaC/scripts/build_nginx.sh
+++ IaC/scripts/build_nginx.sh
@@ -4,6 +4,6 @@
 this_file=$(realpath $0)
 this_dir=$(dirname $this_file)
 # Build the path to the API Application's base directory
-api_dir=$(realpath $this_dir/../../)
+iac_dir=$(realpath $this_dir/..)
 
-docker buildx build --platform=linux/amd64 -t public.ecr.aws/d0s9n5w1/beaver-api:latest $api_dir/backend/apps/beaver_api
+docker buildx build --platform=linux/amd64 -t public.ecr.aws/d0s9n5w1/beaver-nginx:latest $iac_dir/docker/nginx
---
+++ IaC/scripts/push_nginx.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker push public.ecr.aws/d0s9n5w1/beaver-nginx:latest
