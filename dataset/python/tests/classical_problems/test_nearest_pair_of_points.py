c28ad6e6ce7d91a46a79a66656e877df0850cd9b
nikita zigman <zigman.nikita@gmail.com>
2024-09-11 22:38:28 +0100

    Updated python dataset structure

---
+++ dataset/python/tests/classical_problems/test_nearest_pair_of_points.py
@@ -0,0 +1,37 @@
+from math import sqrt
+
+import pytest
+
+from algorithms.classical_problems.nearest_pair_of_points.main import (
+    nearest_points,
+)
+
+
+def test_nearest_points():
+    points = [(0, 0), (1, 1), (4, 4), (5, 5)]
+    result = nearest_points(points)
+    assert result == pytest.approx(sqrt(2), 0.0001)
+
+
+def test_nearest_points_brute_force():
+    points = [(0, 0), (1, 1), (2, 2), (3, 3)]
+    result = nearest_points(points)
+    assert result == pytest.approx(sqrt(2), 0.0001)
+
+
+def test_nearest_points_single_point():
+    points = [(0, 0)]
+    result = nearest_points(points)
+    assert result == float("inf")
+
+
+def test_nearest_points_two_points():
+    points = [(0, 0), (3, 4)]
+    result = nearest_points(points)
+    assert result == pytest.approx(5, 0.0001)
+
+
+def test_nearest_points_no_points():
+    points = []
+    result = nearest_points(points)
+    assert result == float("inf")
---
+++ dataset/python/tests/classical_problems/test_stable_marriage_problem.py
@@ -0,0 +1,10 @@
+from algorithms.classical_problems.stable_marriage_problem.main import (
+    stable_marriage_problem,
+)
+
+
+def test_stable_marriage_problem():
+    man_p = [[0, 1, 2], [0, 1, 2], [0, 1, 2]]
+    girl_p = [[0, 1, 2], [0, 1, 2], [0, 1, 2]]
+    expected_pairs = {0: 0, 1: 1, 2: 2}
+    assert stable_marriage_problem(man_p, girl_p) == expected_pairs
