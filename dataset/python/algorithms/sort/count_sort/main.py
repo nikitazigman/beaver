1555c4e687226980cfde900038b6b3bf18fd3840
nikita zigman <zigman.nikita@gmail.com>
2024-10-22 00:02:52 +0100

    Refactor count sort

--- dataset/python/algorithms/sort/count_sort/main.py
+++ dataset/python/algorithms/sort/count_sort/main.py
@@ -1,12 +1,16 @@
-def count_sort(arr):
+def count_sort(arr: list[int]) -> list[int]:
     if len(arr) <= 1:
         return arr
-    max_element = max(arr)
-    count_map = {}
+
+    max_element: int = max(arr)
+
+    count_map: dict[int, int] = {}
     for element in arr:
         count_map[element] = count_map.get(element, 0) + 1
-    result_array = []
+
+    result_array: list[int] = []
     for i in range(1, max_element + 1):
         if i in count_map:
             result_array.extend([i] * count_map[i])
+
     return result_array
