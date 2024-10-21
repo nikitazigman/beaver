487b4529c8a674a4dd47156827feb234e2442352
nikita zigman <zigman.nikita@gmail.com>
2024-10-22 00:18:49 +0100

    Refactor insertion_sort

--- dataset/python/algorithms/sort/insertion_sort/main.py
+++ dataset/python/algorithms/sort/insertion_sort/main.py
@@ -1,6 +1,8 @@
-def insertion_sort(array):
+def insertion_sort(array: list[int]) -> list[int]:
     for i in range(len(array)):
-        j = i
+        j: int = i
         while j > 0 and array[j] < array[j - 1]:
-            array[j], array[j-1] = array[j-1], array[j]
-            j-=1
+            array[j], array[j - 1] = array[j - 1], array[j]
+            j -= 1
+
+    return array
