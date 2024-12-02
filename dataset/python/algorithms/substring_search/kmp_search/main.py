ade403566f585a7b8be6466a73bd113afdf68c94
nikita zigman <zigman.nikita@gmail.com>
2024-12-02 23:32:11 +0000

    Add kmp substring search

--- dataset/python/algorithms/substring_search/kmp_search/main.py
+++ dataset/python/algorithms/substring_search/kmp_search/main.py
@@ -1,32 +1,32 @@
-def kmp_search(str, substr):
-    offsets = get_offsets(substr)
-    j = 0
-    i = 0
-    while i < len(str):
-        if str[i] == substr[j] and j == len(substr) - 1:
+def kmp_search(input: str, substr: str) -> int:
+    offsets: list[int] = get_offsets(substr=substr)
+    j, i = 0, 0
+
+    while i < len(input):
+        if input[i] == substr[j] and j == len(substr) - 1:
             return i - j
-        elif str[i] == substr[j]:
-            i += 1
-            j += 1
-        elif str[i] != substr[j] and j == 0:
+        elif input[i] == substr[j]:
+            i, j = i + 1, j + 1
+        elif input[i] != substr[j] and j == 0:
             i += 1
         else:
             j = offsets[j - 1]
+
     return -1
 
 
-def get_offsets(substr):
-    offsets = [0]
-    i = 1
-    j = 0
+def get_offsets(substr: str) -> list[int]:
+    offsets: list[int] = [0]
+    i, j = 1, 0
+
     while i < len(substr):
         if substr[i] == substr[j]:
             offsets.append(j + 1)
-            j += 1
-            i += 1
+            i, j = i + 1, j + 1
         elif substr[i] != substr[j] and j == 0:
             offsets.append(j)
             i += 1
         else:
-            j = offsets[j - 1]
+            j: int = offsets[j - 1]
+
     return offsets
