5cdf066801533525dd136debb3dc8adcbad9f746
nikita zigman <zigman.nikita@gmail.com>
2025-03-30 17:03:45 +0100

    Move infra to eu-west-1, switch to use 3rd party db. The ecs uses fargate, remove nginx form task definition, and remove collect static from the start.sh

--- IaC/terraform/load_balancer.tf
+++ IaC/terraform/load_balancer.tf
@@ -1,36 +1,13 @@
-resource "aws_security_group" "http" {
-  name_prefix = "http-sg-"
-  description = "Allow all HTTP/HTTPS traffic from public"
-  vpc_id      = aws_vpc.main.id
-
-  dynamic "ingress" {
-    for_each = [80, 443]
-    content {
-      protocol    = "tcp"
-      from_port   = ingress.value
-      to_port     = ingress.value
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_lb" "main" {
+resource "aws_lb" "production" {
   name               = "${var.ecs_cluster_name}-alb"
   load_balancer_type = "application"
   subnets            = aws_subnet.public[*].id
-  security_groups    = [aws_security_group.http.id]
+  security_groups    = [aws_security_group.load-balancer.id]
 }
 
-resource "aws_lb_target_group" "app" {
+resource "aws_lb_target_group" "default-target-group" {
   name        = "${var.ecs_cluster_name}-tg"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_vpc.production-vpc.id
   protocol    = "HTTP"
   port        = 80
   target_type = "ip"
@@ -39,22 +16,23 @@ resource "aws_lb_target_group" "app" {
     path                = var.health_check_path
     port                = "traffic-port"
     healthy_threshold   = 5
-    unhealthy_threshold = 5
-    timeout             = 20
-    interval            = 30
+    unhealthy_threshold = 2
+    timeout             = 2
+    interval            = 5
     matcher             = "200"
   }
 }
 
+# Listener (redirects traffic from the load balancer to the target group)
 resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.main.id
+  load_balancer_arn = aws_lb.production.id
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = var.certificate_arn
-  depends_on        = [aws_lb_target_group.app]
+  depends_on        = [aws_lb_target_group.default-target-group]
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.app.id
+    target_group_arn = aws_lb_target_group.default-target-group.arn
   }
 }
--- IaC/terraform/network.tf
+++ IaC/terraform/network.tf
@@ -1,11 +1,11 @@
 data "aws_availability_zones" "available" { state = "available" }
 
 locals {
-  azs_count = 2
+  azs_count = length(var.availability_zones)
   azs_names = var.availability_zones
 }
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "production-vpc" {
   cidr_block           = "10.10.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
@@ -13,37 +13,34 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  count                   = local.azs_count
-  vpc_id                  = aws_vpc.main.id
-  availability_zone       = local.azs_names[count.index]
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 10 + count.index)
-  map_public_ip_on_launch = true
-  tags                    = { Name = "public-${local.azs_names[count.index]}" }
+  count             = local.azs_count
+  vpc_id            = aws_vpc.production-vpc.id
+  availability_zone = local.azs_names[count.index]
+  cidr_block        = cidrsubnet(aws_vpc.production-vpc.cidr_block, 8, 10 + count.index) # 10.10.10.0/24
+  tags              = { Name = "public-${local.azs_names[count.index]}" }
 }
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-  tags   = { Name = "igw" }
+
+
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.production-vpc.id
 }
 
-resource "aws_eip" "main" {
-  count      = local.azs_count
-  depends_on = [aws_internet_gateway.main]
-  tags       = { Name = "eip-${local.azs_names[count.index]}" }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-  tags   = { Name = "rt-public" }
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-}
 
 resource "aws_route_table_association" "public" {
   count          = local.azs_count
   subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+
+resource "aws_internet_gateway" "production-igw" {
+  vpc_id = aws_vpc.production-vpc.id
+}
+
+resource "aws_route" "public-internet-igw-route" {
+  route_table_id         = aws_route_table.public-route-table.id
+  gateway_id             = aws_internet_gateway.production-igw.id
+  destination_cidr_block = "0.0.0.0/0"
 }
