f18a8f050d6d77b344c7253e8284f13cc105de84
nikita zigman <zigman.nikita@gmail.com>
2025-04-14 21:55:44 +0100

    Add business layer to contributor domain

---
+++ backend/apps/beaver_api_v2/internal/business/contributorbus/models.go
@@ -0,0 +1,29 @@
+package busconributor
+
+import (
+	"beaver-api/internal/storage/contributordb"
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+type Contributor struct {
+	ID           uuid.UUID
+	Name         string
+	LastName     string
+	EmailAddress string
+}
+
+func toBusContrib(contrib contributordb.Contributor) (Contributor, error) {
+	if !(contrib.ID.Valid || contrib.Name.Valid || contrib.LastName.Valid || contrib.EmailAddress.Valid) {
+		return Contributor{}, errors.New("Some of the Contributor fields are not Valid")
+	}
+
+	contribBus := Contributor{
+		ID:           uuid.UUID(contrib.ID.Bytes),
+		Name:         contrib.Name.String,
+		LastName:     contrib.LastName.String,
+		EmailAddress: contrib.EmailAddress.String,
+	}
+	return contribBus, nil
+}
